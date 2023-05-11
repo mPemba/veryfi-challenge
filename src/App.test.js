cf0925ba1c303e5e14b83785c9da31d2782020b3
mPemba <michael.j.say@gmail.com>
2023-05-11 16:19:56 -0600

    adding basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a header with the text "Receipt Analyzer"', () => {
+  render(<App />);
+  const header = screen.getByText(/Receipt Analyzer/i);
+  expect(header).toBeInTheDocument();
+});
+
+test('renders a button with the text "Upload Receipt"', () => {
+  render(<App />);
+  const button = screen.getByText(/Upload Receipt/i);
+  expect(button).toBeInTheDocument();
 });
